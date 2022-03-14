2120643cd61f2644cf47cb9cebe4333d5dbf2f9e
Mariela-t <91850798+Mariela-t@users.noreply.github.com>
2022-03-13 18:32:28 -0700

    Added search engine

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header"> hello</header>
+      <div className="container">
+        <header className="App-header"> Dictionary</header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">Coded by Mariela Tinoco Rojas</footer>
+      </div>
     </div>
   );
 }

c9b8b38a10648fb730bb2b54d9fa7119b72c02d5
Mariela-t <91850798+Mariela-t@users.noreply.github.com>
2022-03-28 14:11:44 -0700

    styled css

--- src/Photos.js
+++ src/Photos.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./Photos.css";
 
 export default function Photos(props) {
-  console.log(props.photos);
   if (props.photos) {
     return (
       <section className="Photos">
@@ -11,12 +10,21 @@ export default function Photos(props) {
             return (
               <div className="col-4" key={index}>
                 <a href={photo.src.original} target="_blank" rel="noreferrer">
-                  <img src={photo.src.tiny} className="images" />
+                  <img
+                    src={photo.src.tiny}
+                    className="images"
+                    alt={photo.alt}
+                  />
                 </a>
               </div>
             );
           })}
         </div>
+        <footer>
+          <a href="https://www.pexels.com" tabIndex="_blank">
+            Photos provided by Pexels
+          </a>
+        </footer>
       </section>
     );
   } else {
